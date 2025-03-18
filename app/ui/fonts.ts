6e2a58bc9fa9a79e4cab63942ab5067532d11f2d
Carlos Gimenez <krlito.gb@gmail.com>
2025-03-18 16:36:21 -0300

    Commit Inicial

---
+++ app/ui/fonts.ts
@@ -0,0 +1,11 @@
+import { Inter, Montserrat, Lusitana } from "next/font/google";
+
+export const inter = Inter({ subsets: ["latin"] });
+export const montserrat = Montserrat({
+  subsets: ["latin"],
+  weight: ["400", "700"],
+});
+export const lusitana = Lusitana({
+  subsets: ["latin"],
+  weight: ["400", "700"],
+});
